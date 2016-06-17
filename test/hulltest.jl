8240366e36908eb450c43bbd81bc975576bb0fe6
James Noeckel <jwn48@cornell.edu>
2016-06-17 10:48:22 -0700

    added test

--- test/hulltest.jl
+++ test/hulltest.jl
@@ -7,3 +7,8 @@ data = hcat(data1, [100, 0, 0], data2)
 v, ind = convexhull(data, true, 1e-8, true)
 println("volume of hull: $(volume(v, ind))")
 saveObj(v, ind, "output/rand.obj")
+
+data = [1 1 1 1 0 0 0 0; 1 1 0 0 1 1 0 0; 1 0 1 0 1 0 1 0]
+v, ind = convexhull(data)
+println("volume of hull: $(volume(v, ind))")
+println("surface area of hull: $(surfacearea(v, ind))")
